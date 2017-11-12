9c576c8c232a0806aeb472786b5bb48a53567484
Michal Puskas <michalpuskas93@gmail.com>
2017-11-12 16:27:22 +0100

    Added route to register form and  also created template fo it

--- src/routes.php
+++ src/routes.php
@@ -11,18 +11,9 @@ $app->get('/', function (Request $request, Response $response, array $args) {
     $config = $this->config->getConfig();
     $carrot_api = new Api($config);
     $metrics = $carrot_api->getAllDeviceData();
-    // Nazorna ukazka zobrazenia udajov zariadeni
-
-    /*for($i = 0; $i < count($metrics); $i++){
-        echo "<h3> Zaznam" . $i .  "</h3><br>";
-        for ($j = 0; $j < count($metrics[$i]); $j++){
-            echo "<p> cas:" . $metrics[$i][$j]['cas'] . "/<p>";
-            echo "<p> hodnota:" . $metrics[$i][$j]['hodnota'] . "/<p>";
-            echo "<p> typ:" . $metrics[$i][$j]['typ'] . "/<p>";
-            echo "<br>";
-        }
-    }*/
-
-    // Render index view
     return $this->renderer->render($response, 'index.phtml', ['menu' => $request->getAttribute('menu'), 'footer' => $request->getAttribute('footer')]);
+});
+
+$app->get('/register', function (Request $request, Response $response, array $args) {
+    return $this->renderer->render($response, 'register_form.phtml', ['menu' => $request->getAttribute('menu'), 'footer' => $request->getAttribute('footer')]);
 });
