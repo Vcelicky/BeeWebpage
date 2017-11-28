7bce46f1607de2f2533caaa697fe8de39e202937
Michal Puskas <michalpuskas93@gmail.com>
2017-11-28 01:37:42 +0100

    Update login page and added proper response message after submit login button

--- public/js/style.js
+++ public/js/style.js
@@ -4,3 +4,39 @@ window.onload = function() {
   	footer.css("position", "relative");
   }
 };
+
+$("#login_button").click(function () {
+    var loginForm = document.getElementById("login");
+    var email = loginForm["0"].value;
+    var pass = loginForm["1"].value;
+
+    data = {
+        email: email,
+        password : pass
+
+    };
+    $.ajax({
+        url: '../src/login.php',
+        method : 'POST',
+        dataType : 'json',
+        data : data
+    })
+        .done(function (data) {
+            alterLogin(data);
+        });
+});
+
+// alert response after user tried to login to the site
+function alterLogin(data) {
+    if (data.error) {
+        $("#ModalLoginForm .modal-header").append("<div class=\"alert alert-danger\">\n" +
+            "  <strong>Chyba!</strong> " + data.error_msg + "\n" +
+            "</div>");
+
+        setTimeout(function(){
+            var alert = $("#ModalLoginForm .modal-header .alert");
+            alert.fadeOut();
+            alert.remove();
+        }, 5000);
+    }
+}
