90b4b64772498dad00013d4391222558c7a8ce14
7evenco <toma7.koren@gmail.com>
2018-03-06 18:24:49 +0100

    Bootstrapu 4.0.0 Alfa to 4.0.3 Beta at portal. Logout from portal.

--- public/js/portal_main.js
+++ public/js/portal_main.js
@@ -26,6 +26,7 @@
 		$('.search-trigger').parent('.header-left').removeClass('open');
 	});
 
+	// getDevices();
 });
 
 $("#log_out_button").click(function() {
@@ -43,17 +44,38 @@ function deleteCookie(name) {
 }
 
 function logout() {
-    var loc = window.location.origin;
-    $.ajax({
-        url: loc + '/BeeWebpage/public/logout',
-        method : 'POST',
-        dataType : 'json',
-        headers : {
-            'Content-Type' : 'application/json'
-        }
-    }).done(function () {
-        window.location.assign(window.origin + "/BeeWebpage/public");
-    });
+        var loc = window.location.origin;
+        $.ajax({
+            url: loc + '/BeeWebpage/public/logout',
+            method : 'POST',
+            dataType : 'json',
+            headers : {
+                'Content-Type' : 'application/json'
+            }
+        }).done(function () {
+            window.location.assign(window.origin + "/BeeWebpage/public");
+        });
+    }
+
+function getDevices() {
+	var loc = window.location.origin;
+
+    data = {
+        user_id: 45,
+        token : "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjMifQ.NWOjv_uDNmgUU4sYWN3-wkCCjo4d-berGfRWC3FQ-9g"
+
+    };
+
+	$.ajax({
+		url: loc + '/BeeWebpage/public/user/devices',
+        // url:  'http://team20-17.studenti.fiit.stuba.sk/BeeWebpage/public/user/devices',
+		method : 'POST',
+        data : data,
+        contentType:'application/json; charset=utf-8',
+        dataType:'json'
+	}).done(function (data) {
+		console.log(data);
+	});
 }
 
 
