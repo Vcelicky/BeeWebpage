579875846c5094ec33d1c4df97a66e27e678d691
Michal Puskas <michalpuskas93@gmail.com>
2018-04-04 12:46:29 +0200

    add notification to templates and created back-end for notifications management

--- src/Db_api/DbManager.php
+++ src/Db_api/DbManager.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Tomas
- * Date: 11.11.2017
- * Time: 11:25
- */
 
 namespace src\Db_api;
 use \Firebase\JWT\JWT;
@@ -284,140 +278,14 @@ class DbManager
         return $hash;
     }
 
-    public function addNotificationMessage($item) {
-        $returned_message = [
-            "title" => "",
-            "body"  => ""
-        ];
-
-        $message_topic = preg_split('/_/', key($item));
-
-        if (count($message_topic) < 2) {
-            // weight message
-            if (strcmp(key($item), "weight") === 0) {
-                $returned_meessage["title"] = "Prekrocenie hranice hmotnosti";
-                $returned_message["body"]  = "Hmotnost vcelinu je " . $item["weight"]["value"];
-            }
-
-            // battery
-            if (strcmp(key($item), "battery") === 0) {
-                $returned_message["title"] = "Hodnota baterie";
-                $returned_message["body"]  = "Hodnota baterie je " . $item["battery"]["value"];
-            }
-
-            //proximity
-            if (strcmp(key($item), "poloha") === 0) {
-                $returned_message["title"] = "Vcelin sa prevratil";
-                $returned_message["body"]  = "";
-            }
-        }
-        else {
-            $returned_message["title"] = $message_topic[0] . " " . $message_topic[1] . " prekrocila hranicu";
-            $returned_message["body"]  = $message_topic[0] . " ma hodnotu " . $item["battery"]["value"];
-        }
-
-        return $returned_message;
-    }
-
-    public function addNotification($hive_id, $notifications, $time) {
-        $user_id = $notifications["user_id"];
-        $hive_name = $notifications["hive_name"];
-        $error = false;
-        array_pop($notifications);
-        array_pop($notifications);
-        $query =   'INSERT INTO bees.notifications(title_text, body_text, hive_id, hive_name, user_id, time)
-                    VALUES ($1, $2, $3, $4, $5, $6);';
-        $notification_query = pg_prepare($this->conn, "notification insertion", $query);
-
-        foreach ($notifications as $notification) {
-            $message = $this->addNotificationMessage($notification);
-            $result = pg_execute($this->conn, "notification insertion", [
-                $message["title"],
-                $message["body"],
-                $hive_id,
-                $hive_name,
-                $user_id,
-                date('Y-m-d G:i:s', $time)
-            ]);
-
-            if (!$result) {
-                error_log(pg_last_error(), true);
-                $error = true;
-            }
-        }
-
-        return $error;
-
-    }
-    public function checkNotification($data, $device_id) {
-        $result = false;
-
-        $query = 'SELECT  d.user_id,
-                          d.uf_name,
-                          d.weight_limit,
-                          d.temperature_out_down_limit,
-                          d.temperature_out_up_limit,
-                          d.temperature_in_down_limit,
-                          d.temperature_in_up_limit,
-                          d.humidity_out_down_limit,
-                          d.humidity_out_up_limit,
-                          d.humidity_in_down_limit,
-                          d.humidity_in_up_limit,
-                          d.batery_limit								
-                 FROM bees.devices d
-                 WHERE d.device_id = $1;';
-
-        $devices_limits = pg_prepare($this->conn, 'device limits query', $query);
-        $devices_limits = pg_execute($this->conn, 'device limits query', [$device_id]);
-        $limits = pg_fetch_row($devices_limits);
-
-        if ($limits) {
-            $result = [];
-            $devices_limits = pg_prepare($this->conn, 'device limits query', $query);
-            // first check weight and batery
-            if ($data['hmotnost'] > $limits[2]) {
-                array_push($result, ['weight' => ['value' => $data['hmotnost'], 'type' => '']]);
-            }
-            if($data['stav_baterie'] < $limits[count($limits) - 1]) {
-                array_push($result, ['battery' => ['value' => $data['stav_baterie'], 'type' => '']]);
-            }
-
-            // remove checked data rows
-            $data = array_reverse($data);
-            array_pop($data);
-            $data = array_reverse($data);
-            array_pop($data);
-
-            // check up and down limits of temperature and humidity
-            $limit_item = 3;
-            foreach ($data as $key => $data_item) {
-                if ($data_item < $limits[$limit_item]) {
-                    array_push($result, [$key => ['value' => $data_item, 'type' => 'down']]);
-                }
-                $limit_item++;
-                if ($data_item > $limits[$limit_item]) {
-                    array_push($result, [$key => ['value' => $data_item, 'type' => 'up']]);
-                }
-                $limit_item++;
-            }
-
-            //check if any notification was added
-            if (count($result) > 0) {
-                $result['user_id']   = $limits[0];
-                $result['hive_name'] = $limits[1];
-            }
-
-        }
-        return $result;
-    }
-
     public function insertValue($raw_data)
     {
         $data = parser::getData($raw_data['data']['value']);
         // check notifications
-        $notifications = $this->checkNotification($data,  $raw_data['id']);
+        $notification = new DbNotification($this->conn);
+        $notifications = $notification->checkNotification($data,  $raw_data['id']);
         if ($notifications) {
-            $this->addNotification($raw_data['id'], $notifications, $raw_data['time']);
+            $notification->addNotification($raw_data['id'], $notifications, $raw_data['time']);
         }
 
         $query = 'INSERT INTO bees.measurements(time, temperature_in, weight, proximity, temperature_out,
